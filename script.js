2eb80cabb493d3ae68a8a89874eb8273c085f9e8
KelloHitt <venus.cert@gmail.com>
2026-07-23 19:52:22 +1200

    Added an id to each Book object in the constructor of the Book function and implemented the addBookToLibraryfunction

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const myLibrary = [];
 
 // Creating a Book constructor
-function Book(title, author, numOfPages, bookRead) {
+function Book(title, author, numOfPages, bookRead, id) {
   this.title = title;
   this.author = author;
   this.numOfPages = numOfPages;
-  this.bookRead = bookRead;
+  this.bookRead = bookRead ? "read" : "not read yet";
+  this.id = id;
 
   function info() {
     return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.bookRead}`;
@@ -14,4 +15,8 @@ function Book(title, author, numOfPages, bookRead) {
 }
 
 // Creating a book using parameters then sotring it in an array
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, numOfPages, bookRead) {
+  const newBookId = crypto.randomUUID();
+  const newBook = new Book(title, author, numOfPages, bookRead, newBookId);
+  myLibrary.push(newBook);
+}
