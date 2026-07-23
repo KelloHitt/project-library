ca31d9d341417c8f5a7820b5910274998a360c59
KelloHitt <venus.cert@gmail.com>
2026-07-23 20:14:13 +1200

    Changed function in Book constructor to use the this keyword so every object  instance can access the function

--- script.js
+++ script.js
@@ -9,14 +9,28 @@ function Book(title, author, numOfPages, bookRead, id) {
   this.bookRead = bookRead ? "read" : "not read yet";
   this.id = id;
 
-  function info() {
+  this.info() = function () {
     return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.bookRead}`;
   }
 }
 
 // Creating a book using parameters then sotring it in an array
 function addBookToLibrary(title, author, numOfPages, bookRead) {
+  // const is block scoped so it can technically be used as a new execution context is created and a new 'newBookId' is created
   const newBookId = crypto.randomUUID();
   const newBook = new Book(title, author, numOfPages, bookRead, newBookId);
   myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, true);
+
+console.log(myLibrary);
+console.log("Hiiii?");
+
+for (const book in myLibrary) {
+  console.log(book.info());
 }
